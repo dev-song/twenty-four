abb0c0d5729821e96e7503830162ff19b74bbf93
dev_song <52951039+dev-song@users.noreply.github.com>
2020-10-05 16:20:54 +0900

    Feat: add a validation feature for TimeForm.js

--- src/components/TimeForm.js
+++ src/components/TimeForm.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+function validateSchedule(startTime, endTime, todo) {
+  const isStartTimeValid = startTime < 24 && startTime > 0,
+    isEndTimeValid = endTime <= 24 && endTime > startTime,
+    isTodoValid = todo.length <= 24;
+
+  if (!isStartTimeValid || !isEndTimeValid || !isTodoValid) {
+    return false;
+  }
+  return true;
+}
+
 function TimeForm({ addSchedule }) {
   function handleSubmit(event) {
     event.preventDefault();
@@ -8,6 +19,9 @@ function TimeForm({ addSchedule }) {
       endTime = parseInt(event.target[1].value),
       todo = event.target[2].value;
 
+    if (!validateSchedule(startTime, endTime, todo)) {
+      return false;
+    }
     addSchedule(startTime, endTime, todo);
   }
 
